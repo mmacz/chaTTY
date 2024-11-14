2d0c3f03749debb248f5815232930dd96d06d4de
Marek Macznik <marek.macznik@gmail.com>
2024-11-14 22:54:51 +0100

    Add simple server implementation

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,21 +1,67 @@
-use std::net::{TcpListener, Ipv4Addr};
-use clap::Parser;
+use log::{Level, LevelFilter, Metadata, Record, SetLoggerError};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::{broadcast, Mutex};
+use std::sync::Arc;
+use std::collections::HashMap;
+use std::env;
 
-#[derive(Parser, Debug)]
-#[command(author = "mmacz", version="1.0.0", about = None, long_about = None)]
-struct Args {
-    #[arg(short, long, default_value_t = { Ipv4Addr::new(127, 0, 0, 1) } )]
-    ip: Ipv4Addr,
+mod client_handler;
+use client_handler::{ClientList, handle_client};
 
-    #[arg(short, long, default_value_t = 8080)]
-    port: u16,
+struct SimpleLogger;
 
-    #[arg(short, long, default_value_t = ("chaTTY".to_string()))]
-    name: String
+impl log::Log for SimpleLogger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::Info
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            println!("{} - {}", record.level(), record.args());
+        }
+    }
+
+    fn flush(&self) {}
 }
 
-fn main() {
-    let args = Args::parse();
-    println!("Hello server: {:?}", args);
-}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    static LOGGER: SimpleLogger = SimpleLogger;
+    log::set_logger(&LOGGER)
+        .map(|()| log::set_max_level(LevelFilter::Info))
+        .expect("Failed to initialize logger!");
 
+    let port: u16 = match env::var("CHATTY_PORT") {
+        Ok(p) => p.parse().unwrap(),
+        _ => 8080,
+    };
+
+    log::info!("Starting server on port: {}", port);
+
+    match TcpListener::bind(format!("127.0.0.1:{}", port)).await {
+        Ok(listener) => {
+            log::info!("Server started!");
+            let (broadcast_tx, _) = broadcast::channel::<String>(1);
+            let clients: ClientList = Arc::new(Mutex::new(HashMap::new()));
+
+            loop {
+                let (socket, addr) = listener.accept().await?;
+                log::info!("Client connected: {}", addr);
+
+                let tx = broadcast_tx.clone();
+                let clients = Arc::clone(&clients);
+
+                tokio::spawn(async move {
+                    handle_client(socket, tx, clients).await;
+                });
+            }
+        }
+        Err(e) => {
+            log::error!("Could not start a chaTTY server! {}", e);
+            return Err(e.into());
+        }
+    }
+
+    Ok(())
+}
